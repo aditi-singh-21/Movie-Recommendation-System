2f3bc47e197cf6fb0f0b20d3c2340071e0a062a8
aditi-singh-21 <aditisinghrk906@gmail.com>
2024-07-08 14:03:02 +0530

    new

--- app/movie_recc_sys/views.py
+++ app/movie_recc_sys/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from .models import Movie
+from .input_movie import similar
 
 # Create your views here.
